0a3a10118cca144b9a8b516b8814bcb4e38cd0fc
GHF Soulless <ghf.soulless@gmail.com>
2024-09-02 17:51:34 -0400

    Fixed UI for ghf_reporter

--- GHF Toolkit/ghf_reporter.bat
+++ GHF Toolkit/ghf_reporter.bat
@@ -1,6 +1,6 @@
 @echo off
 title GHF Reporter
 netsh wlan show wlanreport
-echo Report complete, press any key to close PGV Toolkit 2.1.
+echo Report complete, press any key to close GHF Toolkit 2.1.
 pause
 exit /B
